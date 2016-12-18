67baf7b2c73241a4fe313b96abc525d0172530ab
ide_r <ide_r@om.tose.co.jp>
2016-12-18 15:32:18 +0900

    UE4のC++プロジェクトを作成
    登る用のMovementクラスを追加。

---
+++ proj/Source/proj/Public/ClimbMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/MovementComponent.h"
+#include "ClimbMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API UClimbMovementComponent : public UMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
