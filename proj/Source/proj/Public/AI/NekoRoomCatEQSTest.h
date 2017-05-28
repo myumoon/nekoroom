dcb1937a311a1b16c4127f25e2553d332e50a6c7
ryunosuke_ide <ryumyu0120@gmail.com>
2017-05-28 18:00:25 +0900

    猫のジャンプ地点を判断するためのEQSを追加
    目標地点から到達可能なジャンプ地点を検索していき
    現在位置からの最短ジャンプ地点を返していくようにする予定。

---
+++ proj/Source/proj/Public/AI/NekoRoomCatEQSTest.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EnvironmentQuery/EnvQueryTest.h"
+#include "NekoRoomCatEQSTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API UNekoRoomCatEQSTest : public UEnvQueryTest
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
