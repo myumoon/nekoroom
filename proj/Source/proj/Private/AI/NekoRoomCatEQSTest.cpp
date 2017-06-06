5d06ace4eb53dc327cf92a256b7c8a200c88f57b
ryunosuke_ide <ryumyu0120@gmail.com>
2017-06-06 21:06:16 +0900

    猫が移動する位置の計算準備
    指定箇所から一番近いポイントを取得。

--- proj/Source/proj/Private/AI/NekoRoomCatEQSTest.cpp
+++ proj/Source/proj/Private/AI/NekoRoomCatEQSTest.cpp
@@ -5,6 +5,7 @@
 #include "proj.h"
 #include "NekoRoomCatEQSTest.h"
 #include "EnvironmentQuery/Items/EnvQueryItemType_VectorBase.h"
+#include "Runtime/Core/Public/Math/Box.h"
 
 //-----------------------------------------------------------------
 /*	@brief	コンストラクタ
@@ -15,8 +16,9 @@ UNekoRoomCatEQSTest::UNekoRoomCatEQSTest(const FObjectInitializer& ObjectInitial
 	// testのコスト？
 	Cost = EEnvTestCost::Low;
 
-	// アイテムは何のパラメーターを受け取れるか。多分。
+	// アイテムは何のパラメーターを受け取れるか。
 	ValidItemType = UEnvQueryItemType_VectorBase::StaticClass();
+
 }
 
 //-----------------------------------------------------------------
@@ -29,9 +31,10 @@ void UNekoRoomCatEQSTest::RunTest(FEnvQueryInstance& queryInstance) const
 	if(queryOwner == nullptr) {
 		return;
 	}
-
-	//FName name("FinalPosX");
-	//queryInstance.NamedParams[]
+	
+	FBox    box     = queryInstance.GetBoundingBox();
+	FVector size    = box.GetSize();
+	float   maxEdge = FMath::Max3(size.X, size.Y, size.Z);
 
 
 	//UNavigationSystem* navSys = UNavigationSystem::GetCurrent(GetWorld());
@@ -50,6 +53,12 @@ void UNekoRoomCatEQSTest::RunTest(FEnvQueryInstance& queryInstance) const
 	FVector finalPos(FinalPosX.GetValue(), FinalPosY.GetValue(), FinalPosZ.GetValue());
 	UE_LOG(LogTemp, Warning, TEXT("finalPos (%f, %f, %f)"), finalPos.X, finalPos.Y, finalPos.Z);
 
+	const AActor* nearest = GetNearestPoint(finalPos, queryInstance);
+	if(nearest == nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("Not found nearest point"));
+	}
+	//CalcWeight(, maxEdge, finalPos);
+
 	//FPathFindingQuery queryParam;
 	//queryParam.StartLocation = ;
 	//queryParam.EndLocation = ;
@@ -62,11 +71,49 @@ void UNekoRoomCatEQSTest::RunTest(FEnvQueryInstance& queryInstance) const
 		
 		UE_LOG(LogTemp, Log, TEXT("it %d"), it.GetIndex());
 		//const AActor* point = queryInstance.GetItemAsActor(it.GetIndex());
-		score += 0.10f;
+		score += 0.10f; // todo : 仮で設定
 		it.SetScore(TestPurpose, FilterType, score, FloatValueMin.GetValue(), FloatValueMax.GetValue());
 	}
 }
 
+//-----------------------------------------------------------------
+/*	@brief	一番近いポイントを取得
+*/
+//-----------------------------------------------------------------
+const AActor* UNekoRoomCatEQSTest::GetNearestPoint(const FVector& pos, const FEnvQueryInstance& queryInstance) const
+{
+	TArray<AActor*> points = TArray<AActor*>();
+	queryInstance.GetAllAsActors(points);
+
+	if(points.Num() == 0) {
+		return nullptr;
+	}
+
+	AActor* nearest      = points[0];
+	float   nearestDist2 = (pos - nearest->GetActorLocation()).SizeSquared();
+
+	// 先頭はすでにnearestに入っているので除外
+	for(int i = 1; i < points.Num(); ++i) {
+		AActor* point  = points[i];
+		FVector length = pos - nearest->GetActorLocation();
+		float   dist2  = length.SizeSquared();
+		if(dist2 < nearestDist2) {
+			nearest      = point;
+			nearestDist2 = dist2;
+		}
+	}
+
+	return nearest;
+}
+
+//-----------------------------------------------------------------
+/*	@brief	移動コスト計算
+*/
+//-----------------------------------------------------------------
+float UNekoRoomCatEQSTest::CalcWeight(const FVector& pos, float maxEdge, const FVector& finalPos) const
+{
+	return ((pos - finalPos).Size() / maxEdge);
+}
 
 FText UNekoRoomCatEQSTest::GetDescriptionTitle() const
 {
--- proj/Source/proj/Public/AI/NekoRoomCatEQSTest.h
+++ proj/Source/proj/Public/AI/NekoRoomCatEQSTest.h
@@ -5,8 +5,10 @@
 #include "EnvironmentQuery/EnvQueryTest.h"
 #include "NekoRoomCatEQSTest.generated.h"
 
-/**
- * 
+/*!	@brief  猫の移動位置計算用EQSテスト
+	
+	登れる壁がある場合にスコアが高くなる。
+	猫が好きなものが置いてある場所もスコアが高くなる予定。
  */
 UCLASS()
 class PROJ_API UNekoRoomCatEQSTest : public UEnvQueryTest
@@ -17,17 +19,28 @@ class PROJ_API UNekoRoomCatEQSTest : public UEnvQueryTest
 	float CanJumpRange;    // ジャンプできる距離
 
 	UPROPERTY(EditAnywhere, Category = TestParam)
-	FAIDataProviderFloatValue FinalPosX;
+	FAIDataProviderFloatValue FinalPosX; // 最終目的地X
 
 	UPROPERTY(EditAnywhere, Category = TestParam)
-	FAIDataProviderFloatValue FinalPosY;
+	FAIDataProviderFloatValue FinalPosY; // 最終目的地Y
 
 	UPROPERTY(EditAnywhere, Category = TestParam)
-	FAIDataProviderFloatValue FinalPosZ;
+	FAIDataProviderFloatValue FinalPosZ; // 最終目的地Z
 	
-	// テスト実行
+	/*!	@brief	テスト実行
+	*/
 	virtual void RunTest(FEnvQueryInstance& queryInstance) const override;
 
 	virtual FText GetDescriptionTitle() const override;
 	virtual FText GetDescriptionDetails() const override;
+
+private:
+	/*! @brief   指定場所に一番近いオブジェクトを取得
+	*/
+	const AActor* GetNearestPoint(const FVector& point, const FEnvQueryInstance& queryInstance) const;
+
+	/*! @brief   FinalPosX,Y,Zからの移動コストを計算
+		@return  コスト（0～1）：0ほど移動量が少ない
+	*/
+	float CalcWeight(const FVector& pos, float maxEdge, const FVector& finalPos) const;
 };
